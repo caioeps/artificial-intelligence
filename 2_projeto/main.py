c14cd360cf8365cfd82bbb968c2fcbc38c5fe5dc
Caio <caioergos@gmail.com>
2019-06-23 01:32:46 -0300

    Finish MQ with PCA

--- 2_projeto/main.py
+++ 2_projeto/main.py
@@ -1,31 +1,60 @@
 import sys
 import os
 import mimetypes
-from PIL import Image
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import statistics as st
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 sys.path.append(os.path.dirname(__file__))
 
 from classifiers import MQClassifier
-from preprocessing import PCA
+from preprocessing import load_data, get_images, get_subject_numbers
 
-def get_image(subject, expression):
-    image = Image.open("./YALE-A/%s.%s" % (subject, expression))
-    image = image.resize((50, 50), Image.ANTIALIAS)
-    return np.array(image)
+def mq_pca():
+    P_success = []
+    for i in range(0, 20):
+        print('Iteration:', i)
+        data = load_data()
+        subjects = get_subject_numbers(data)
+        images = get_images(data)
 
-faces = []
-for _, _, files in os.walk("./YALE-A"):
-    for filename in files:
-        if filename.startswith('subject'):
-            faces.append(filename)
-faces = [face.split('.') for face in faces]
+        X_train, X_test, y_train, y_test = train_test_split(images, subjects, test_size = 0.2, random_state = i)
 
-images = [[[subject, expression], get_image(subject, expression)] for subject, expression in faces]
+        scaler = StandardScaler()
+        scaler.fit(X_train)
 
-print(images[0][1].shape)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
 
+        pca = PCA(.95)
+        pca.fit(X_train)
+        X_train = pca.transform(X_train)
+        X_test = pca.transform(X_test)
 
+        mq = MQClassifier()
+        mq.fit(X_train, y_train)
+
+        y_pred = mq.predict(X_test)
+        y_pred = [int(round(x)) for x in y_pred]
+        n_success = [y_pred[i] for i in range(0, len(y_pred)) if y_pred[i] == y_test[i]]
+        n_success = len(n_success)
+
+        p_success = n_success / len(y_test) * 100
+        P_success.append(p_success)
+
+    print(P_success)
+    P_success_min = min(P_success)
+    P_success_max = max(P_success)
+    P_success_mean = st.mean(P_success)
+    P_success_std = st.stdev(P_success)
+
+    print("P_success_min: ", P_success_min)
+    print("P_success_mean: ", P_success_mean)
+    print("P_success_std: ", P_success_std)
+
+mq_pca()
 
--- 2_projeto/preprocessing.py
+++ 2_projeto/preprocessing.py
@@ -1,1 +1,37 @@
-from sklearn.decomposition import PCA
+import os
+import re
+from PIL import Image
+import numpy as np
+
+__data_dir = "%s/YALE-A/" % os.path.dirname(__file__)
+
+def load_data():
+    faces = []
+    for _, _, files in os.walk(__data_dir):
+        for filename in files:
+            if filename.startswith('subject'):
+                faces.append(filename)
+    faces = [face.split('.') for face in faces]
+    data = [[subject, expression, __get_image(subject, expression)] for subject, expression in faces]
+    return data
+
+def get_images(data):
+    images = [image for _, __, image in data]
+    images = np.array(images)
+    return images;
+
+def get_subject_numbers(data):
+    subjects =  [__get_subject_number(d) for d in data]
+    subjects = np.array(subjects)
+    return subjects
+
+def __get_subject_number(data_entry):
+    subject, _, __ = data_entry
+    subject_number = re.sub('\D', '', subject)
+    return int(subject_number)
+
+def __get_image(subject, expression):
+    image = Image.open("%s/%s.%s" % (__data_dir, subject, expression))
+    image = image.resize((50, 50), Image.ANTIALIAS)
+    return np.array(image).flatten()
+
